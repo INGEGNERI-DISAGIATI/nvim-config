70f231d6f0c0d896f97a22f4f88de0f281280a99
Tommischiri <tommischiri@gmail.com>
2024-10-19 15:56:28 +0200

    Bho

--- init.lua
+++ init.lua
@@ -14,3 +14,12 @@ for _, name in ipairs(core_conf_files) do
   vim.cmd(source_cmd)
 end
 
+--vim.cmd("nnoremap <C-I> :w<CR> :echo system('gcc -o nvimoutput ' . expand('%') . ' && ./nvimoutput && rm ./nvimoutput')<CR>")
+--vim.cmd("nnoremap <C-I> :w<CR> :!gcc -o nvimoutput % && ./nvimoutput && rm ./nvimoutput<CR>")
+
+--vim.cmd("nnoremap <C-I> :w<CR>:!gcc -o nvimoutput % && stdbuf -o0 ./nvimoutput && rm ./nvimoutput<CR>")
+vim.cmd("nnoremap <C-I> :w<CR>:!gcc -o nvimoutput %<CR><ENTER>:vert term ./nvimoutput<CR>:!rm ./nvimoutput<CR><ENTER>:startinsert<CR>")
+
+
+vim.fn.setreg('i',vim.api.nvim_replace_termcodes('<Esc>gg0i#include <stdio.h><CR>#include <stdbool.h><CR>#include <string.h><CR><CR>int main(void) {<CR><CR>}<Up><Tab><Esc>',true,false,true))
+
